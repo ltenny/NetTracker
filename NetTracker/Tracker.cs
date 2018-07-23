b076527e91f623f0838fecc6b4763d855c37f5b7
Larry Tenny <ltenny@gmail.com>
2018-07-23 07:54:33 -0500

    Updated with bad actors list

--- NetTracker/Tracker.cs
+++ NetTracker/Tracker.cs
@@ -28,6 +28,8 @@ namespace NetTracker
         private DatabaseReader reader;
 
         private List<string> possibleDBLocations = new List<string> { "c:\\GeoLite2-Country.mmdb", "GeoLite2-Country.mmdb" };
+        private Dictionary<string, bool> badActors;
+
         #region Public Methods
 
         public Tracker()
@@ -40,6 +42,7 @@ namespace NetTracker
             AddLocalAddressPrefixes();
             table = new Dictionary<string, DateTime>();
             lastWrite = DateTime.Now;
+            InitBadActorsTable();
 
             foreach(var possible in possibleDBLocations)
             {
@@ -79,6 +82,20 @@ namespace NetTracker
             }
         }
 
+        public bool IsBadActor(string addr)
+        {
+            try
+            { 
+                var cc = reader.Country(addr).Country.IsoCode;
+                if (string.IsNullOrEmpty(cc)) return false;
+                return (badActors.ContainsKey(cc));
+            }
+            catch(Exception)
+            {
+                return false; ;
+            }
+        }
+
         public void PacketHandler(Packet packet)
         {
             IpV4Datagram ip = packet.Ethernet.IpV4;
@@ -90,6 +107,66 @@ namespace NetTracker
         #endregion // Public Methods
 
         #region Private Methods
+
+        private void InitBadActorsTable()
+        {
+            // might be easier to to whitelist the good guys -;)
+            badActors = new Dictionary<string, bool>();
+            badActors.Add("AF", true);
+            badActors.Add("AL", true);
+            badActors.Add("AO", true);
+            badActors.Add("AR", true);
+            badActors.Add("AZ", true);
+            badActors.Add("BY", true);
+            badActors.Add("BJ", true);
+            badActors.Add("BG", true);
+            badActors.Add("KH", true);
+            badActors.Add("HR", true);
+            badActors.Add("EE", true);
+            badActors.Add("GA", true);
+            badActors.Add("GM", true);
+            badActors.Add("GN", true);
+            badActors.Add("GW", true);
+            badActors.Add("IR", true);
+            badActors.Add("CI", true);
+            badActors.Add("KZ", true);
+            badActors.Add("KE", true);
+            badActors.Add("KI", true);
+            badActors.Add("KG", true);
+            badActors.Add("LA", true);
+            badActors.Add("LV", true);
+            badActors.Add("LS", true);
+            badActors.Add("LR", true);
+            badActors.Add("LT", true);
+            badActors.Add("MW", true);
+            badActors.Add("MR", true);
+            badActors.Add("MU", true);
+            badActors.Add("MN", true);
+            badActors.Add("MZ", true);
+            badActors.Add("NR", true);
+            badActors.Add("NP", true);
+            badActors.Add("NI", true);
+            badActors.Add("KP", true);
+            badActors.Add("OM", true);
+            badActors.Add("PK", true);
+            badActors.Add("PS", true);
+            badActors.Add("RU", true);
+            badActors.Add("RO", true);
+            badActors.Add("RS", true);
+            badActors.Add("SC", true);
+            badActors.Add("SL", true);
+            badActors.Add("SK", true);
+            badActors.Add("SY", true);
+            badActors.Add("TJ", true);
+            badActors.Add("TH", true);
+            badActors.Add("TN", true);
+            badActors.Add("UA", true);
+            badActors.Add("UZ", true);
+            badActors.Add("VN", true);
+            badActors.Add("YE", true);
+            badActors.Add("ZM", true);
+            badActors.Add("ZW", true);
+        }
 
         private void AddLocalAddressPrefixes()
         {
@@ -114,21 +191,32 @@ namespace NetTracker
         }
         private void AddToTable(DateTime ts, string addr)
         {
+            string fmt = "yyyy-MM-dd-hh-mm-ss-fff";
+            string fmt_file = "yyyy-MM-dd hh:mm:ss";
+
             if (!IgnoreAddressFilter(addr))
             {
                 if (!table.ContainsKey(addr))
                 {
                     table.Add(addr, ts);
-                    if (IsNotInUSA(addr))
+                    if (IsBadActor(addr))
                     {
-                        Console.WriteLine($"New address: {addr} in country {GetCountry(addr)}");
+                        var save = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{ts.ToString(fmt_file)}: New address: {addr} in country {GetCountry(addr)}");
+                        Console.ForegroundColor = save;
+                    }
+                    else
+                    {
+                        if (IsNotInUSA(addr))
+                        {
+                            Console.WriteLine($"{ts.ToString(fmt_file)}: New address: {addr} in country {GetCountry(addr)}");
+                        }
                     }
                 }
             }
-            if (table.Count > 0 && (ts - lastWrite) > TimeSpan.FromMinutes(LogFrequency))
+            if ((ts - lastWrite) > TimeSpan.FromMinutes(LogFrequency))
             {
-                string fmt = "yyyy-MM-dd-hh-mm-ss-fff";
-                string fmt_file = "yyyy-MM-dd hh:mm:ss";
                 string fullPath = Path.Combine(LogPath, $"{ts.ToString(fmt)}.txt");
                 using (StreamWriter writer = new StreamWriter(fullPath))
                 {
